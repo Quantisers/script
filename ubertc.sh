6a9f8ff78a31234577dd90a6d50fc7da10a8df4f
Vishnuvardhan Reddy <39486314+Quantummech2000@users.noreply.github.com>
2018-06-28 09:49:07 +0530

    Fix Compilation Error

--- ubertc.sh
+++ ubertc.sh
@@ -1,4 +1,2 @@
 make clean && make mrproper
-export ARCH=arm64 && export CROSS_COMPILE=~/aarch64-linux-android-7.0-kernel && export KBUILD_BUILD_USER=QuantumMechanic && export KBUILD_BUILD_HOST=TEAMQUANTUM && mkdir -p out && make O=out santoni_defconfig && make O=out -j$(nproc --all)
-
-
+export ARCH=arm64 && export CROSS_COMPILE=~/aarch64-linux-android-7.0-kernel/aarch64-linux-android && export KBUILD_BUILD_USER=QuantumMechanic && export KBUILD_BUILD_HOST=TEAMQUANTUM && mkdir -p out && make O=out santoni_defconfig && make O=out -j$(nproc --all)
